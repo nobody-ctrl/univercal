7b201057046d06431d41dfccf320771bd5fa7ecd
ctrl-nobody <par.12092000@gmail.com>
2021-01-22 19:38:24 +0300

    many fixes

--- js/main.js
+++ js/main.js
@@ -16,18 +16,25 @@ $(".right-block__text-header").each(function(i) {
 
 $('.menu').click(function(){
     $('.mobile').toggleClass('mobile-visible');
+    $('body').toggleClass('body-overflow-hidden');
 });
 
 var mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
-    loop: true,
-
+    lazy: true,
+    autoplay: {
+        delay: 3000,
+      },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: 'true'
     },
 })
+
+$('.news__book').on("click", function() {
+    $(this).children(":first").toggleClass('news__book-color-red');
+});
 
 $('.newsletter__form').validate({
     errorClass: "message-box",
@@ -37,4 +44,43 @@ $('.newsletter__form').validate({
           email: "Please, correct email!"
         }
     }    
+});
+
+$('.right-block__link').click(function(){
+
+    var header = $(this).children().eq(0).children().eq(0);
+    var headerColor = header.css('color');
+    var descr = $(this).children().eq(0).children().eq(1);
+
+    $('.text-block__first').text(header.text());
+    $('.text-block__first').css({'color' : headerColor});
+
+    $('.text-block__second').text(descr.text());
+
+    var names = ['Оксана Фомина', 'Станислав Гордеев', 'Ирина Фельдман', 'Мария Жукова', 'Игорь Поддубный'];
+    var titles = ['Фотограф', 'Путешественник', 'Разработчик', 'Блогер', 'Студент'];
+    var number = 0;
+    if(headerColor == 'rgb(59, 189, 196)'){
+        number = 0;
+        $('.hero__image').attr({'src' : '../img/hero-five.jpg'});
+        $('.avatar-block__img').attr({'src' : '../img/one.jpg'});
+    }else if(headerColor == 'rgb(110, 153, 174)'){
+        number = 1;
+        $('.hero__image').attr({'src' : '../img/hero-second.png'});
+        $('.avatar-block__img').attr({'src' : '../img/two.jpg'});
+    }else if (headerColor == 'rgb(172, 142, 227)'){
+        number = 2;
+        $('.hero__image').attr({'src' : '../img/hero-first.jpg'});
+        $('.avatar-block__img').attr({'src' : '../img/three.jpg'});
+    }else if(headerColor == 'rgb(255, 163, 77)'){
+        number = 3;
+        $('.hero__image').attr({'src' : '../img/hero-four.jpg'});
+        $('.avatar-block__img').attr({'src' : '../img/four.jpg'});
+    }else{
+        number = 4;
+        $('.hero__image').attr({'src' : '../img/hero-first.jpg'});
+        $('.avatar-block__img').attr({'src' : '../img/five.jpg'});
+    }
+    $('.avatar-block__text-one').text(names[number]);
+    $('.avatar-block__text-two').text(titles[number]);
 });
